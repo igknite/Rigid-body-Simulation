70d3341660142cad43d0d8220750fbd144ab1351
igknite <yi9704@naver.com>
2019-09-23 21:46:27 +0900

    Upload project skeleton code

---
+++ OpenGL_skeleton/OpenGL_skeleton/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include <glut.h>
+#include <Box2D.h>
+
+
+//Function declaraions
+void display();
+void keyboard(unsigned char k, int x, int y);
+void mouse(int button, int state, int x, int y);
+void reshape(int w, int h);
+
+
+//------------------------------------------------------------------------------
+int main(int argc, char* argv[]) {
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+	glutCreateWindow("OpenGL");
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
+	glutReshapeFunc(reshape);
+
+	glutMainLoop();
+
+	return 0;
+}
+//------------------------------------------------------------------------------
+void display()
+{
+	
+
+
+}
+//------------------------------------------------------------------------------
+void keyboard(unsigned char key, int x, int y)
+{
+	
+
+
+}
+//-----------------------------------------------------------------------------
+void mouse(int button, int state, int mx, int my)
+{
+	
+
+
+}
+//------------------------------------------------------------------------------
+void reshape(int w, int h)
+{
+	
+
+
+}
